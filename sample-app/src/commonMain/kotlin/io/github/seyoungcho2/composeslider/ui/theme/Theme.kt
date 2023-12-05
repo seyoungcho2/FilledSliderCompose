7de8edce1cb5ac6f950c8e1ddd61484d4aee713d
IRACK (CHAEUN) <IRACK000@gmail.com>
2023-12-05 16:21:58 +0900

    feat: Add Target-Specified Theme Code

--- sample-app/src/commonMain/kotlin/io/github/seyoungcho2/composeslider/ui/theme/Theme.kt
+++ sample-app/src/commonMain/kotlin/io/github/seyoungcho2/composeslider/ui/theme/Theme.kt
@@ -1,27 +1,19 @@
 package io.github.seyoungcho2.composeslider.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
+val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
     tertiary = Pink80
 )
 
-private val LightColorScheme = lightColorScheme(
+val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
@@ -44,27 +36,16 @@ fun ComposeSliderTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme                                                      -> DarkColorScheme
-        else                                                           -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
-
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = getAppThemeColor(darkTheme, dynamicColor),
         typography = Typography,
         content = content
     )
 }
+
+@Composable
+expect fun getAppThemeColor(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true
+): ColorScheme
--- sample-app/src/commonMain/kotlin/io/github/seyoungcho2/composeslider/ui/theme/Type.kt
+++ sample-app/src/commonMain/kotlin/io/github/seyoungcho2/composeslider/ui/theme/Type.kt
@@ -1,22 +1,18 @@
 package io.github.seyoungcho2.composeslider.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
-import io.github.seyoungcho2.composeslider.R
 
-val robotoFontFamily = FontFamily(
-    Font(R.font.roboto_regular, FontWeight.Normal, FontStyle.Normal)
-)
+
+expect fun getAppFontFamily(): FontFamily?
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = robotoFontFamily,
+        fontFamily = getAppFontFamily(),
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
