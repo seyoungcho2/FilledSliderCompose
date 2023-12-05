cbbcb19d1b93308336d72390130c6a53c775cf2e
IRACK (CHAEUN) <IRACK000@gmail.com>
2023-12-05 15:18:50 +0900

    chore: Fix ClassCastException while Build
    Upgrade Compose Compiler Version from 1.5.0 to 1.5.3
    
    @Ref: https://github.com/JetBrains/compose-multiplatform/issues/3531

--- buildSrc/src/main/kotlin/version/LibraryVersions.kt
+++ buildSrc/src/main/kotlin/version/LibraryVersions.kt
@@ -6,7 +6,7 @@ object LibraryVersions {
     val appCompat = "1.6.1"
 
     // Compose
-    val composeComplier = "1.5.0"
+    val composeComplier = "1.5.3"
     val activityCompose = "1.7.1"
 
     // Lifecycle
